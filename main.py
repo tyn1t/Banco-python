22811d7a1c9e8e021ee716e1f4ed7fa6bc1914f8
Giovanni <anderlansouza71@gmail.com>
2025-04-26 19:18:21 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+from models.db import db
+import datetime
+from Operecoes.operecoes import Operecoes
+
+menu = """
+    [d] => depositar 
+    [S] => Sacar 
+    [E] => Extrato
+    [1] => Limpar Terminal 
+    [0] => Sair 
+    
+=== > 
+"""
+
+def limpar_terminal():
+    return os.system("cls" if os.name == "nt" else "clear")
+
+
+
+Oper = Operecoes()
+
+
+
+
+nome = input('Nome: ')
+if db.usuario_existe(nome):
+    print("continua ...")
+else:
+    print("Banco BPY")
+    print('Cadastra ...')
+    nome = input('Nome: ')
+    idade = int(input('Idade: '))
+    db.db_insert_User(name=nome, age=idade)
+    
+user = db.db_mostra_user(nome)
+user_id = user[0][0]
+
+
+date = datetime.datetime.now()
+
+if user_id:
+    db.db_insert_Extrato(saldo=Oper.saldo, date=date, user_id=user_id)
+
+while True:
+    
+    print("Bem Banco BPY ...")
+    opcoes = input(menu).lower()
+    
+    print(f"User:  {user[0][1]:<15}  |  Saldo: R$ {Oper.saldo:,.2f}")
+    
+    if opcoes == "d": 
+        print("Depositar")
+        valor = input('Valor:')
+        Oper.Depositar(value=float(valor))
+        db.db_insert_Extrato(saldo=Oper.saldo, date=datetime.datetime.now(), user_id=user_id)
+
+        
+    if opcoes == "s":
+        print("Sacar")
+        valor = input('Valor:')
+        Oper.Sacar(value=float(valor))
+        db.db_insert_Extrato(saldo=Oper.saldo, date=datetime.datetime.now(), user_id=user_id)
+
+        
+    
+    if opcoes == "e": 
+        print("Extrato")
+        extratos = db.mostra_extrato(user_id=user_id)  # Atualiza!
+        for extrato in extratos:
+            print(f'ID: {extrato[0]} | Saldo: {extrato[1]}  | Date: {extrato[2]}')
+    
+    if opcoes == "0":
+        print("Sair ...")
+        break
+    
+    if opcoes == "1":
+        limpar_terminal()
---
+++ models/db.py
@@ -0,0 +1,96 @@
+import sqlite3
+
+
+class DB:
+    
+    def __init__(self):
+        self.connection = sqlite3.connect('models/database.db')
+        self.cursor = self.connection.cursor()
+
+    def db_create(self):
+        create_table_query = '''
+            CREATE TABLE IF NOT EXISTS User (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                age INTEGER
+            );
+
+            CREATE TABLE IF NOT EXISTS Extrato (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                saldo FLOAT NOT NULL,
+                date TEXT NOT NULL,
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES User(id)
+            );
+                
+        '''
+    
+        self.cursor.executescript(create_table_query)
+        
+        self.connection.commit()
+    
+        print("Database created and connected successfully!")
+        
+    
+    def  db_insert_User(self, name, age):
+        try: 
+            self.cursor.execute(
+                'INSERT INTO User (name, age) VALUES (?, ?)', (name, age))
+            self.connection.commit() 
+        except sqlite3.Error as e:
+            return e
+            
+        
+    def db_insert_Extrato(self, saldo, date, user_id):
+        try:
+            self.cursor.execute(
+                'INSERT INTO Extrato (saldo, date, user_id) VALUES (?,  ?, ?)', (saldo, date, user_id)
+            )
+            self.connection.commit() 
+        except sqlite3.Error as e:
+            return e
+
+        
+    def db_update_user_age(self, user_id, new_age):
+        try:
+            self.cursor.execute(
+                'UPDATE User SET age = ? WHERE id = ?', (new_age, user_id)
+            )
+            self.connection.commit()
+            print("User updated successfully!")
+        except sqlite3.Error as e:
+            return e
+    
+    def db_mostra_user(self, name):
+        try:
+            self.cursor.execute(
+                'SELECT * FROM User WHERE name = ?', (name,)
+            )
+            result = self.cursor.fetchall()
+            return result
+        except sqlite3.Error as e:
+            return e
+    
+    def mostra_extrato(self, user_id):
+        try:
+            self.cursor.execute(
+                'SELECT * FROM Extrato WHERE user_id = ?', (user_id,)
+            )
+            return self.cursor.fetchall()
+        except sqlite3.Error as e:
+            return e
+    
+    def usuario_existe(self, name):
+        try: 
+            self.cursor.execute(
+                'SELECT id FROM User WHERE name = ?', (name,)
+            )
+            
+            return self.cursor.fetchone()
+        
+        except sqlite3.Error as e:
+            return e
+            
+
+db = DB()
+db.db_create()
